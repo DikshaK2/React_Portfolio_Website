8f65a5c48f37ad536c765e7d8c0c2a72cfead802
DikshaK2 <dikshak225@gmail.com>
2024-08-16 13:56:47 +0530

    project

---
+++ my-project1/src/components/WorkCardData.js
@@ -0,0 +1,38 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.jpg"
+import pro3 from "../assets/project3.jpg"
+import pro4 from "../assets/project4.jpg"
+
+const ProjectCardData=[
+    {
+        imgsrc:pro1,
+        title:"Ecommerce Website Design",
+        text:"Thriving Facebook community and top-ranked podcast, amy inspires a grounded,tangible and self-affirming sense of 'Wow! I really can do this' for over 100,000 online enterpreneurs.Best-selling marketing courses",
+
+        view:"https://www.youtube.com/watch?v=0h2b4ftbZcU&t=7453s"
+    },
+    {
+        imgsrc:pro2,
+        title:"LMS/Course Website Design",
+        text:"Thriving Facebook community and top-ranked podcast, amy inspires a grounded,tangible and self-affirming sense of 'Wow! I really can do this' for over 100,000 online enterpreneurs.Best-selling marketing courses",
+
+        view:"https://www.youtube.com/watch?v=0h2b4ftbZcU&t=7453s"
+    },
+    {
+        imgsrc:pro3,
+        title:"Rymo Ecommerce Design",
+        text:"Thriving Facebook community and top-ranked podcast, amy inspires a grounded,tangible and self-affirming sense of 'Wow! I really can do this' for over 100,000 online enterpreneurs.Best-selling marketing courses",
+
+        view:"https://www.youtube.com/watch?v=0h2b4ftbZcU&t=7453s"
+    },
+    {
+        imgsrc:pro4,
+        title:"Rymo Ecommerce Design",
+        text:"Thriving Facebook community and top-ranked podcast, amy inspires a grounded,tangible and self-affirming sense of 'Wow! I really can do this' for over 100,000 online enterpreneurs.Best-selling marketing courses",
+
+        view:"https://www.youtube.com/watch?v=0h2b4ftbZcU&t=7453s"
+    }
+    
+];
+
+export default ProjectCardData;
